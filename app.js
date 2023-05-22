fc0b08ebbf96936dfeb49e65b122a289c87beec9
MABGuiza <benguizameda@proton.me>
2023-05-22 16:12:03 +0100

    added healthcheck and auto deploy trigger

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get("/version", (req, res) => {
-  res.send("0.00"); // change this string to ensure a new version deployed
+  res.send("0.01"); // change this string to ensure a new version deployed
+});
+
+app.get("/health", (req, res) => {
+  res.send("ok").status(200);
 });
 
 app.listen(PORT, () => {
